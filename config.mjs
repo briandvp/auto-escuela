c72c075d76109d360d75e76b3660524a51a35b19
Brian Gamarra <brian7u7ramdon@gmail.com>
2026-02-23 15:00:50 -0300

    testimonials

---
+++ config.mjs
@@ -0,0 +1,39 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.miautoescuela.es/',
+  output: 'static',
+  integrations: [
+    sitemap()
+  ],
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  compressHTML: true,
+  vite: {
+    build: {
+      cssMinify: true,
+      minify: 'esbuild',
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['astro']
+          }
+        }
+      }
+    },
+    css: {
+      transformer: 'lightningcss'
+    }
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  },
+  experimental: {
+    clientPrerender: true
+  }
+});
